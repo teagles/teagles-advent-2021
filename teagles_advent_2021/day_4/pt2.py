995958e90ca7a8ba935f047420c6f506039ec8d8
Tim Eagles <tim.eagles.au@gmail.com>
2021-12-03 23:46:38 -0800

    Day 4 part 2 complete.

--- teagles_advent_2021/day_4/pt2.py
+++ teagles_advent_2021/day_4/pt2.py
@@ -1,8 +1,27 @@
 import sys
 
+from .lib import BingoBoard, numerals_from_input
+
 
 def main():
-    print()
+    game_input = sys.stdin.readlines()
+    game_numbers = map(int, game_input[0].split(','))
+    boards = []
+    stream = iter(game_input[1:])
+    while True:
+        board = numerals_from_input(stream)
+        if board:
+            boards.append(BingoBoard(board))
+        else:
+            break
+    for number in game_numbers:
+        for board in boards.copy():
+            result = board.play_numeral(number)
+            if result:
+                boards.remove(board)
+                if not boards:
+                    print(result)
+                    return
 
 
 if __name__ == '__main__':
